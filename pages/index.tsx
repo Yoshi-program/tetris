25c0b185e4eff5165639f7001b2f94ecbf62c0be
s1F102103303 <s1F102103303@iniad.org>
2022-02-22 17:20:52 +0900

    枠組みはできた

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,18 @@ import type { NextPage } from 'next'
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import styled from 'styled-components'
 
-const COLORS = ['', 'lightblue', 'blue', 'orange', 'yellow', 'lightgreen', 'purple', 'red']
+const COLORS = [
+  'black',
+  'lightblue',
+  'blue',
+  'orange',
+  'yellow',
+  'lightgreen',
+  'purple',
+  'red',
+  '',
+  'black',
+]
 
 const Container = styled.div`
   height: 100vh;
@@ -18,7 +29,7 @@ const AroundBlockArea = styled.div`
   margin-right: -50%;
   background-color: #cdcdcd;
   border: solid 5px;
-  border-color: #fff #3e3e3e #3e3e3e #fff;
+  border-color: #fff #777 #777 #fff;
   transform: translate(-50%, -50%);
 `
 const Board = styled.div`
@@ -37,16 +48,8 @@ const MinBlock = styled.div<{ num: number }>`
   height: 30px;
   line-height: 30px;
   vertical-align: baseline;
-  background-color: ${(props) =>
-    props.num >= 1 && props.num <= 7 ? COLORS[props.num] : '#000000'};
+  background-color: ${(props) => COLORS[props.num]};
   border: solid 0.15vh #000;
-`
-const HideBlock = styled.div`
-  float: left;
-  width: 30px;
-  height: 30px;
-  line-height: 30px;
-  vertical-align: baseline;
 `
 const Test = styled.div`
   text-align: center;
@@ -82,34 +85,38 @@ const Home: NextPage = () => {
   ]
   const [start, gameStart] = useState(false)
   const [resetBlock, ResetBlock] = useState(false)
-  const [checkReset, CheckReset] = useState(false)
-  const [tetromino, createTetromino] = useState(BLOCKS[Math.floor(Math.random() * 6) + 1])
-  const [before, beforeBoard] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //const [checkReset, CheckReset] = useState(false)
+  const [tetromino, createTetromino] = useState(BLOCKS[Math.floor(Math.random() * 7)])
+  const before = [
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    //---
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
     //ここから表示しない
-    [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-  ])
+    [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+  ]
   const [board, setBoard] = useState(before)
-  const [x, X] = useState(3)
+  const [x, X] = useState(4)
   const [y, Y] = useState(0)
   // 一つ一つのブロックの情報
   const [block, setBlock] = useState({
@@ -128,75 +135,96 @@ const Home: NextPage = () => {
     return memoResult
   }*/
 
-  const reset = () => {
-    ResetBlock(false)
-    CheckReset(false)
-    //beforeBoard(board)
-    //console.log(board) //なぜか一度にたくさん表示される
-    createTetromino(BLOCKS[Math.floor(Math.random() * 6) + 1])
-    X(3)
-    Y(0)
-    setBlock({ y: y, x: x, blockIndex: tetromino })
-    //console.log(resetBlock)
-    //console.log(resetBlock)
-  }
-  const handleKeyDown = useCallback((e: KeyboardEvent) => {
-    //const keyCode = e.keyCode
-    //let test = false
-    //while (!test) {
-    if (e.key === 'ArrowLeft') {
-      //if (1 <= x && x <= 9) {
-      //X(x - 1)
-      X((c) => c - 1)
-      console.log(x)
-      //return
-      //}
-    }
-    //console.log('左')
-    else if (e.key === 'ArrowRight') {
-      //if (0 <= x && x <= 6) {
-      //X(x + 1)
-      X((c) => c + 1)
-      console.log(x)
-      //}
-      //return
-      //console.log('右')
-    } else if (e.key === 'ArrowDown') {
-      Y((c) => c + 1)
-      //return
-      //console.log('下')
-    }
-  }, [])
-
-  /*useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown, false)
-  }, [x, y])*/
-  // 下に進めるかどうかを判定する関数
-  const checkUnder = () => {
-    const newBoard: number[][] = JSON.parse(JSON.stringify(before))
-    for (const cy of [y, y + 1]) {
-      for (const cx of [x, x + 1, x + 2]) {
-        if (
-          newBoard[cy][cx] === newBoard[cy + 1][cx] &&
-          newBoard[cy][cx] !== before[cy + 1][cx] &&
-          newBoard[cy][cx] !== 0
-        ) {
-          console.log('sss')
-          continue
-        }
-        if (
-          (1 <= newBoard[cy][cx] &&
-            newBoard[cy][cx] <= 7 &&
-            1 <= newBoard[cy + 1][cx] &&
-            newBoard[cy + 1][cx] <= 9) ||
-          newBoard[cy + 1][cx] === 9
-        ) {
-          ResetBlock(true)
-          console.log(resetBlock)
+  const changeBoard = () => {
+    const newBoard: number[][] = JSON.parse(JSON.stringify(board))
+    for (let cy = 0; cy < tetromino.length; cy++) {
+      for (let cx = 0; cx < tetromino[cy].length; cx++) {
+        if (tetromino[cy][cx] !== 0) {
+          newBoard[cy + y][cx + x] = tetromino[cy][cx]
         }
       }
     }
-    return resetBlock
+    /*const check = checkUnder()
+    if (check) {
+      //setBoard(newBoard)
+      reset()
+      return before
+    }*/
+    //if (0 <= y && y <= 19 && 0 <= x && x <= 7) {
+    //テトリミノが０でなければ表示（0で上乗せしない)<---なくてもいいかも
+    /*newBoard[y][x] = newBoard[y][x] === 0 ? tetromino[0][0] : newBoard[y][x]
+    newBoard[y][x + 1] = newBoard[y][x + 1] === 0 ? tetromino[0][1] : newBoard[y][x + 1]
+    newBoard[y][x + 2] = newBoard[y][x + 2] === 0 ? tetromino[0][2] : newBoard[y][x + 2]
+    newBoard[y + 1][x] = newBoard[y + 1][x] === 0 ? tetromino[1][0] : newBoard[y + 1][x]
+    newBoard[y + 1][x + 1] = newBoard[y + 1][x + 1] === 0 ? tetromino[1][1] : newBoard[y + 1][x + 1]
+    newBoard[y + 1][x + 2] = newBoard[y + 1][x + 2] === 0 ? tetromino[1][2] : newBoard[y + 1][x + 2]*/
+    //console.log(before)
+    //}
+    //setBoard(newBoard)
+    return newBoard
+  }
+
+  const completeBoard = useMemo(
+    () =>
+      changeBoard()
+        .slice(3, 23)
+        .map((e) => e.filter((num) => num !== 9)),
+    [x, y, tetromino]
+  )
+  const reset = () => {
+    ResetBlock(false)
+    //console.log(completeBoard)
+    setBoard(changeBoard())
+    //setBoard(completeBoard)
+    //beforeBoard(board)
+    //console.log(board) //なぜか一度にたくさん表示される
+    createTetromino(BLOCKS[Math.floor(Math.random() * 7)])
+    X(4)
+    Y(3)
+    //setBlock({ y: y, x: x, blockIndex: tetromino })
+    //console.log(resetBlock)
+  }
+
+  // 下に進めるかどうかを判定する関数
+  const checkUnder = (cx: number, cy: number, tetromino: number[][]) => {
+    //const newBoard: number[][] = JSON.parse(JSON.stringify(completeBoard))
+    if (cy + tetromino.length > 22) {
+      return true
+    }
+    //console.log(board)
+    const nowBoard = changeBoard()
+    for (const y of [cy, cy + 1]) {
+      for (const x of [cx, cx + 1, cx + 2]) {
+        if (
+          nowBoard[y][x] === nowBoard[y + 1][x] &&
+          nowBoard[y][x] !== board[y + 1][x] &&
+          nowBoard[y][x] !== 0
+        ) {
+          //console.log('sss')
+          continue
+        }
+        if (
+          1 <= nowBoard[y][x] &&
+          nowBoard[y][x] <= 7 &&
+          1 <= nowBoard[y + 1][x] &&
+          nowBoard[y + 1][x] <= 9
+        ) {
+          return true
+        }
+      }
+    }
+
+    /*for (const cy of [y, y + 1]) {
+      for (const cx of [x, x + 1, x + 2]) {
+        if (newBoard[cy + 1][cx] === 9) {
+          return true
+          ResetBlock(true)
+          console.log(resetBlock)
+          //return true
+        }
+      }
+    }*/
+    return false
   }
 
   /*const createBoard = useMemo(() => {
@@ -206,33 +234,6 @@ const Home: NextPage = () => {
     return beforeBoard
   }, [before, block])
   createBoard*/
-
-  const changeBoard = () => {
-    const newBoard: number[][] = JSON.parse(JSON.stringify(before))
-    const check = checkUnder()
-    if (check) {
-      //setBoard(newBoard)
-      reset()
-      return before
-    }
-    //if (0 <= y && y <= 19 && 0 <= x && x <= 7) {
-    //テトリミノが０でなければ表示（0で上乗せしない)<---なくてもいいかも
-    newBoard[y][x] = newBoard[y][x] === 0 ? tetromino[0][0] : newBoard[y][x]
-    newBoard[y][x + 1] = newBoard[y][x + 1] === 0 ? tetromino[0][1] : newBoard[y][x + 1]
-    newBoard[y][x + 2] = newBoard[y][x + 2] === 0 ? tetromino[0][2] : newBoard[y][x + 2]
-    newBoard[y + 1][x] = newBoard[y + 1][x] === 0 ? tetromino[1][0] : newBoard[y + 1][x]
-    newBoard[y + 1][x + 1] = newBoard[y + 1][x + 1] === 0 ? tetromino[1][1] : newBoard[y + 1][x + 1]
-    newBoard[y + 1][x + 2] = newBoard[y + 1][x + 2] === 0 ? tetromino[1][2] : newBoard[y + 1][x + 2]
-    console.log(board)
-    //}
-    setBoard(newBoard)
-    return newBoard
-  }
-
-  const completeBoard = useMemo(
-    () => changeBoard().map((e) => e.filter((b) => b !== 9)),
-    [x, y, resetBlock]
-  )
 
   /*useEffect(() => {
     const newBoard: number[][] = JSON.parse(JSON.stringify(before))
@@ -301,16 +302,17 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     //const newBoard: number[][] = JSON.parse(JSON.stringify(before))
-    document.addEventListener('keydown', handleKeyDown, false)
     const interval2 = setInterval(() => {
-      if (resetBlock) {
+      /*if (resetBlock) {
         return
-      }
-      const check = checkUnder()
+      }*/
+      const check = checkUnder(x, y, tetromino)
       if (check) {
         reset()
+        return
+      } else if (!check) {
+        Y((c) => c + 1)
       }
-      Y((c) => c + 1)
 
       //}
       //console.log(before[19][5])
@@ -341,11 +343,11 @@ const Home: NextPage = () => {
       }
       if (resetBlock) {
         Y((c) => c + 1)
-      }
-      console.log(before)*/
+      }*/
+      //console.log(board)
     }, 1000)
     return () => clearInterval(interval2)
-  }, [x, y, resetBlock])
+  }, [y])
 
   //------------
 
@@ -374,27 +376,56 @@ const Home: NextPage = () => {
     if
     const [board, setBoard] = useState(before)
   }, [before])*/
+  //矢印キーで落ちるときの判定処理関数
+  const drop = () => {
+    let yy = y
+    if (!checkUnder(x, y, tetromino)) {
+      yy++
+      console.log(y)
+    }
+    Y(yy)
+  }
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      //const keyCode = e.keyCode
+      //let test = false
+      //while (!test) {
+      if (e.key === 'ArrowLeft') {
+        //if (1 <= x && x <= 9) {
+        //X(x - 1)
+        X((c) => c - 1)
+        console.log(x)
+        return
+        //}
+      }
+      //console.log('左')
+      else if (e.key === 'ArrowRight') {
+        //if (0 <= x && x <= 6) {
+        //X(x + 1)
+        X((c) => c + 1)
+        console.log(x)
+
+        return
+        //console.log('右')
+      } else if (e.key === 'ArrowDown') {
+        drop()
+        //console.log('下')
+      }
+    },
+    [x, y, tetromino]
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown, false)
+  }, [])
 
   return (
     <Container>
       <AroundBlockArea>
         <Board>
           {completeBoard.map((row, y) =>
-            row.map((num, x) =>
-              num === 0 ? (
-                <MinBlock
-                  key={`${x}-${y}`}
-                  num={0 <= num && num <= 7 ? num : 20}
-                  //onClick={() => onClick()}
-                ></MinBlock>
-              ) : (
-                <MinBlock
-                  key={`${x}-${y}`}
-                  num={1 <= num && num <= 7 ? num : 20}
-                  //onClick={() => onClick()}
-                ></MinBlock>
-              )
-            )
+            row.map((num, x) => <MinBlock key={`${x}-${y}`} num={num}></MinBlock>)
           )}
         </Board>
       </AroundBlockArea>
